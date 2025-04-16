c8173dbe32cd0a10241675fbf85a307a41928439
leonardo <leonardocanteros2001@gmail.com>
2025-04-16 18:56:47 -0300

    se actualizan unas view se agregan rutas en el controlador home

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -4,8 +4,51 @@ namespace App\Controllers;
 
 class Home extends BaseController
 {
-    public function index(): string
+    /*public function index(): string
     {
         return view('principal.html');
-    }
+    }*/
+
+    public function index()
+	{
+		echo view('Views/head_view');
+		echo view('Views/navbar_view');
+		echo view('Views/principal_view');
+		echo view('Views/footer_view');
+	}
+
+    public function quienes_somos()
+	{
+		echo View('Views/head_view');
+		echo view('Views/navbar_view');
+		echo view('Views/quienesSomos');
+		echo view('Views/footer_view');
+	}
+/*
+    public function acerca_de()
+	{
+		echo view('front/head_view');
+		echo view('front/navbar_view');
+		echo view('front/acerca_de');
+		echo view('front/footer_view');
+	}
+
+	public function registro()
+	{
+		echo view('front/head_view');
+		echo view('front/navbar_view');
+		echo view('front/registro');
+		echo view('front/footer_view');
+	}
+
+	public function login()
+	{
+		echo view('front/head_view');
+		echo view('front/navbar_view');
+		echo view('front/login');
+		echo view('front/footer_view');
+	}
+
+*/
+
 }
---
+++ app/Views/footer_view.php
@@ -0,0 +1,20 @@
+<footer class="footer">
+        <div class="links" > 
+            <a href="https://www.linkedin.com/in/leonardo-oscar-canteros-03916223a/" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/128/3536/3536505.png" alt="IMG">
+            </a>
+            <a href="https://github.com/leonardo-canteros" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/128/11104/11104255.png" alt="IMG">
+            </a>
+            <a href="mailto:leonardocanteros2001@gmail.com" target="_blank">
+                <img src="https://cdn-icons-png.flaticon.com/128/5968/5968534.png" alt="IMG">
+            </a>
+        </div>
+        <p>Derechos De Autor ©2024 | CUBO_Entertainment. </p>
+    </footer>
+        <script src="assets/js/bootstrap.bundle.min.js" > </script>
+    
+  </body>
+
+
+</html>

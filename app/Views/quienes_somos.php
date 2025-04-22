c39dc6d7172894f781e1dfeeb90ffbf0aadd8c0c
joaquin <juaquin2320003@hotmail.com>
2025-04-22 19:08:28 -0300

    se agregaron imagenes

--- app/Views/quienes_somos.php
+++ app/Views/quienes_somos.php
@@ -1,36 +1,33 @@
 <div class="container quienes-somos">
         <section class="vision-empresa">
-            <p class="vision-texto">Soñamos con un mundo donde cada mascota reciba el amor, el respeto y el cuidado que merece. 
-                Nuestra visión es ser un lugar de confianza y calidez, donde el vínculo entre humanos y animales se fortalezca cada día a través 
-                de una atención médica ética, amorosa y comprometida. Nos esforzamos por ser una clínica que no solo cure, sino
-                 que también abrace, acompañe y eduque a las familias que eligen compartir su vida con seres tan especiales.
-                 Queremos crecer junto a nuestras comunidades, creando un entorno donde cada latido, cada ronroneo, cada mirada de ternura
-                 , tenga un lugar seguro para sanar y sonreír.</p>
+            <p class="vision-texto">La verdadera elegancia no se trata solo de lo que vistes, sino de cómo te haces sentir cuando te lo pones. En nuestra colección
+              , cada prenda ha sido cuidadosamente diseñada para ofrecerte una combinación perfecta de sofisticación, confort y estilo. <br> Ya sea para una ocasión especial o para transformar tu día a día en una experiencia llena de lujo, te invitamos a descubrir la colección que te hará sentir como una obra de arte viviente. Cada detalle cuenta, y tú mereces vestir lo mejor.
+             </p>
         </section>
 
         <section class="equipo">
             <h2 class="titulo-seccion">Nuestro Equipo</h2>
             <div class="card-group-dueño">
                 <div class="card">
-                  <img src="/proyecto_taller/assets/img/dueñodos.jpg" class="card-img-top" alt="Lautaro Gimenez">
+                  <img src="/proyecto_taller/assets/img/messiteamo.jpg" class="card-img-top" alt="Jack Reacher">
                   <div class="card-body">
-                    <h5 class="card-title">Lautaro Gimenez</h5>
-                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                    <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <h5 class="card-title">Jack Reacher</h5>
+                    <p class="card-text">Jack siempre fue un amante del diseño y la moda desde joven, pero lo que más le fascinaba era la ropa que nunca pasa de moda. Después de años de trabajar con diseñadores de alta costura y de viajar por el mundo para conocer las mejores tendencias internacionales, Jack decidió fundar su propia tienda de ropa elegante. Su pasión por lo atemporal y lo sofisticado lo llevó a crear un lugar donde los clientes pudieran encontrar prendas que los hicieran sentir especiales en cualquier ocasión.</p>
+                    <p class="card-text"><small class="text-body-secondary">Last updated 5 mins ago</small></p>
                   </div>
                 </div>
                 <div class="card">
-                  <img src="/proyecto_taller/assets/img/dueñouno.jpg" class="card-img-top" alt="Juan Cruz Galarza">
+                  <img src="/proyecto_taller/assets/img/riquelme.jpg" class="card-img-top" alt="Victor Romanov">
                   <div class="card-body">
-                    <h5 class="card-title">Juan Cruz Galarza</h5>
+                    <h5 class="card-title">Victor Romanov</h5>
                     <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
                     <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                   </div>
                 </div>
                 <div class="card">
-                  <img src="/proyecto_taller/assets/img/dueñotres.jpg" class="card-img-top" alt="Theo Echeverria">
+                  <img src="/proyecto_taller/assets/img/idolo.jpg" class="card-img-top" alt="Victor Quincose">
                   <div class="card-body">
-                    <h5 class="card-title">Theo Echeverria</h5>
+                    <h5 class="card-title">Victor</h5>
                     <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
                     <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
                   </div>

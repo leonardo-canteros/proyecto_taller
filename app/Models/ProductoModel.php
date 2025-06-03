488434a9a1d99f4e9666f1c118e0246916ef64fd
joaquin <juaquin2320003@hotmail.com>
2025-06-03 20:10:32 -0300

    se agrega el crud de productos

---
+++ app/Models/ProductoModel.php
@@ -0,0 +1,26 @@
+<?php namespace App\Models;
+use CodeIgniter\Model;
+
+class ProductoModel extends Model
+{
+    protected $table = 'productos';
+    protected $primaryKey = 'id_producto';
+    protected $useSoftDeletes = true; // Habilita soft delete
+    protected $deletedField = 'deleted_at'; // Campo para marcar eliminación
+    
+    protected $allowedFields = [
+        'nombre', 'descripcion', 'precio', 'stock', 
+        'talla', 'color', 'imagen_url', 'estado', 'id_categoria','deleted_at'
+    ];
+
+    
+
+    // reglas de validación
+    protected $validationRules = [
+        'nombre' => 'required|min_length[3]',
+        'precio' => 'required|decimal',
+        'stock' => 'required|integer'
+    ];
+}
+
+

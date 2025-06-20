ad148496af9f22bffc7701a8b065a7acbe52acd3
joaquin ferretto <joaquin2320003@hotmail.com>
2025-06-20 18:41:45 -0300

    nada

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -53,7 +53,7 @@ class AuthController extends BaseController
         ]);
     } else {
         // Redirigir según el rol
-        return redirect()->to($user['rol'] === 'administrador' ? '/admin' : '/usuario');
+        return redirect()->to($user['rol'] === 'administrador' ? '/admin/principal' : '/usuario');
     }
 }
 
